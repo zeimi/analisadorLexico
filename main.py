7305d7315772e066035490226f39ce29829128c7
s2Alive <rodrigocorreiaba753@gmail.com>
2022-09-01 20:48:35 -0300

    v 2.0

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ import re
 # Lista do nome dos tokens
 
 def listaTokens(info):
-    for i in str(info):
+
+    valores = info.split(" ")
+    
+    for i in valores:
         if i == "(":
-            print(f'O token "{i}" é AP')
+            print(f'A entrada "{i}" é o token AP')
         elif i == ")":
-            print(f'O token "{i}" é FP')
+            print(f'A entrada "{i}" é o token FP')
         elif i == "+":
-            print(f'O token "{i}" é SOM')
+            print(f'A entrada "{i}" é o token SOM')
         elif i == "-":
-            print(f'O token "{i}" é SUB')
+            print(f'A entrada "{i}" é o token SUB')
         elif i == "*":
-            print(f'O token "{i}" é MUL')
+            print(f'A entrada "{i}" é o token MUL')
         elif i == "/":
-            print(f'O token "{i}" é DIV')
+            print(f'A entrada "{i}" é o token DIV')
         elif i == " ":
             continue
-        elif re.search("0|1|2|3|4|5|6|7|8|9", i):
-            print(f'O token "{i}" é NUM')
+        elif re.match("0|1|2|3|4|5|6|7|8|9", i):
+            print(f'A entrada "{i}" é um token NUM')
         else:
-            print (f'O vagabundo "{i}" não é valido')
-    return ("Analise concluida")
+            print (f'A entrada "{i}" não é um token válido')
+    return ("Análise concluida")
 
 
 # Teste do campo de batalha
-entrada = "y = x/2"
+entrada = "a + 3,2 + ( 2 * 12,01 )"
 
 print (listaTokens(entrada))
